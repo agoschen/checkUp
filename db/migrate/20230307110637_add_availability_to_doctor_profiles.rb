9d4439c4e97da0d64a9f7a57d90b42991d6c4fd0
MoJafa <jmohsa@gmail.com>
2023-03-07 17:46:47 +0200

    minor changes on home page

--- db/migrate/20230307110637_add_availability_to_doctor_profiles.rb
+++
@@ -1,5 +0,0 @@
-class AddAvailabilityToDoctorProfiles < ActiveRecord::Migration[7.0]
-  def change
-    add_column :doctor_profiles, :availability, :date
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,65 +1,66 @@
-puts "Starting database"
-puts "Cleaning databas.....e"
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+# For this seed we will be using:
+# - faker to generate names and addresses
+# - open-uri to download images from the internet
+# - active storage to attach the images to the records
+
+require "open-uri"
+
+puts "Cleaning database..."
+
+Appointment.destroy_all
 DoctorProfile.destroy_all
 User.destroy_all
-Appointment.destroy_all
 
-p "Creating users...."
+puts "Creating users..."
 
-20.times do
+# Create 10 users
+10.times do
+
+  # Create the user
   user = User.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
     email: Faker::Internet.email,
-    phone_number: Faker::PhoneNumber.cell_phone,
     password: Faker::Internet.password
+    # password_confirmation: Faker::Internet.password
   )
 
-  p "Creating users: name:#{user[:first_name]} phone number:#{user[:phone_number]}"
-end
+  # Create the doctor profile
 
-puts "done creating users"
-puts "-" * 20
-puts "CREATING DOCTOS"
-puts "-" * 20
+  #  ok i need the users to have images
+  #  i need to create a doctor profile for each user
+  #  i need to create 5 appointments for each doctor profile
+  #  i need to create 5 appointments for each user
 
-5.times do
-  user = User.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    address: Faker::Address.full_address,
-    email: Faker::Internet.email,
-    phone_number: Faker::PhoneNumber.cell_phone,
-    password: Faker::Internet.password
-  )
 
-  puts "-" * 20
-  puts "CREATING USERS FOR doctors: name:#{user[:first_name]} phone number:#{user[:phone_number]}"
-
-  puts "This is user id::: #{user.first_name}"
-
-  doctor_profiles = DoctorProfile.create(
-    specialty: Faker::Job.key_skill,
+  doctor_profile = DoctorProfile.create!(
+    user: user,
+    specialty: Faker::Job.field,
     practice_address: Faker::Address.full_address,
-    availability: Faker::Date.between(from: 1.year.ago, to: 1.year.from_now),
-    user_id: user[:id]
+    availability: Faker::Date.between(from: 1.year.ago, to: 1.year.from_now)
   )
 
-  puts "Creating doctor profiles: specialty:#{doctor_profiles[:specialty]} user id::#{doctor_profiles[:user_id]}"
-  puts "-" * 20
-  puts "CREATING APOINTMENTS"
-  puts "-" * 20
-
-
-  apointments = Appointment.create!(
-    date: Faker::Date.between(from: 1.year.ago, to: 1.year.from_now),
-    start_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
-    end_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
-    status: true,
-    user_id: user[:id],
-    doctor_profile_id: doctor_profiles[:id]
-  )
-  puts "THIS IS THE APPOINTMENTS DATE::: #{apointments.date}"
-  puts "-" * 20
+  # Create 5 appointments
+  5.times do
+    Appointment.create!(
+      user: user,
+      doctor_profile: doctor_profile,
+      date: Faker::Date.between(from: 1.year.ago, to: 1.year.from_now),
+      start_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+      end_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+      status: Faker::Boolean.boolean
+    )
+  end
 end
+
+puts "Finished!"
