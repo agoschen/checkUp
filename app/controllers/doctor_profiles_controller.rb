05aab0536bb8faaae83759be670ae5d028cf2648
MoJafa <jmohsa@gmail.com>
2023-03-08 16:38:40 +0200

    fixed image not showing on home page

--- app/controllers/doctor_profiles_controller.rb
+++ app/controllers/doctor_profiles_controller.rb
@@ -20,8 +20,8 @@ class DoctorProfilesController < ApplicationController
       {
         lat: doctor_profile.latitude,
         lng: doctor_profile.longitude,
-        info_window_html: render_to_string(partial: "info_window", locals: {doctor_profile: @doctor_profile}),
-        marker_html: render_to_string(partial: "marker", locals: {doctor_profile: @doctor_profile})
+        info_window_html: render_to_string(partial: "info_window", locals: {doctor_profile: doctor_profile}),
+        marker_html: render_to_string(partial: "marker", locals: {doctor_profile: doctor_profile})
       }
     end
   end
