9e9d8c3ef827261db6f6709802205bf610e05895
Abigail Goschen <123146410+agoschen@users.noreply.github.com>
2023-03-09 12:56:58 +0200

    updated schema with yonela's changes- note

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_103700) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_105201) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_103700) do
     t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "doctor_note"
+    t.text "note"
     t.index ["doctor_profile_id"], name: "index_appointments_on_doctor_profile_id"
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end
