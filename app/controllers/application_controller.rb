36723c13aba075e2bcf164585d3ebd33bf2bbcad
Yonela Johannes <johannesyonela@gmail.com>
2023-03-09 11:30:38 +0200

    accepting changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-  protect_from_forgery with: :null_session
-  # before_action :authenticate_user!
-=======
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -13,5 +9,4 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name,:address, :phone_number, :photo])
   end
->>>>>>> 8a10ecda7b82d2c40fe96b73f41aa6804078b861
 end
