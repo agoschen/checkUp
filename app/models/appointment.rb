687670b888fac4b6cc14efceb2eb10a46a989833
JessJiggs <verheuljess@gmail.com>
2023-03-09 02:27:44 +0200

    pushing seed file and some changes to my index and show pages as they relate to the Appts controller

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,5 @@
 class Appointment < ApplicationRecord
   belongs_to :user
-  belongs_to :doctor_profile
+  belongs_to :doctor_profile # and shouldnt this be somehow accessible via doctor_profile.user_id
+  # can we possibly add an association here for "avaialbility" - through doctor_profile?
 end
--- db/migrate/20230307103228_add_availability_to_doctor_profiles.rb
+++ db/migrate/20230307103228_add_availability_to_doctor_profiles.rb
@@ -1,5 +1,7 @@
 class AddAvailabilityToDoctorProfiles < ActiveRecord::Migration[7.0]
   def change
     add_column :doctor_profiles, :availability, :date
+    # What if we make availability an integer - and then create an array of slots - the availability - would pull from
+    # the slots array? you could have 2 arrays - one for unbooked slots, and one for booked slots if you needed the flexibility.
   end
 end
